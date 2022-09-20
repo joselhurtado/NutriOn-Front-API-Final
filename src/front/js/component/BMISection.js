ffb366f3cc461babaf48b08794046be2c9a9d374
Jose Hurtado <hello@hurtadojose.com>
2022-09-20 16:23:38 +0000

    BMI calculator testing

--- src/front/js/component/BMISection.js
+++ src/front/js/component/BMISection.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import BMICalculator from "./BMICalculator";
 
 export const BMISection = () => {
   return (
@@ -41,42 +42,20 @@ export const BMISection = () => {
           </div>
         </div>
         <div className="col-sm-5 mx-auto">
-            <div className="card text-center" style={{ maxWidth: "25rem" }}>
-                <div className="card-body">
-                <div className="row m-2">
-                    <h1 className="card-title m-2">Calculate Your BMI</h1>
-                        <div className="col">
-                            <form className="form" id="form" onsubmit="return validateForm()">
-                                <div className="row-one">
-                                    <input type="text" class="text-input" id="age" autocomplete="off" required/><p className="text">Age</p>
-                                    <label className="container">
-                                    <input type="radio" name="radio" id="f" /><p className="text">Female</p>
-                                        <span className="checkmark"></span>
-                                    </label>
-                                    <label className="container">
-                                    <input type="radio" name="radio" id="m" /><p className="text">Male</p>
-                                        <span className="checkmark"></span>
-                                    </label>
-                                </div>
-                                <div class="row-two">
-                                    <input type="text" className="text-input" id="height" autocomplete="off" required /><p className="text">Height (cm)</p>
-                                    <input type="text" className="text-input" id="weight" autocomplete="off" required /><p className="text">Weight (kg)</p>
-                                </div>
-                                <h5 href="#" className="btn btn-orange m-4 ">
-                                  Compute BMI <span className="fa fa-arrow-right"></span>
-                                </h5>
-                            </form>
+          <div className="card text-center" style={{ maxWidth: "25rem" }}>
+            <div className="card-body">
+              <div className="row m-2">
+                <h1 className="card-title m-2">Calculate Your BMI</h1>
+                <div className="col">
+                  <BMICalculator />
                 </div>
+              </div>
             </div>
-
-            <h1>Your Weight</h1>
-            <h1>22.5</h1>
-            </div>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-    </div>
-);
+  );
 };
 
 export default BMISection;
